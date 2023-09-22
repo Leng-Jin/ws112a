cb27f9327239dc34a1fa616418a363555f0f43a3
Leng-Jin <s111110554@student.nqu.edu.tw>
2023-09-22 10:03:17 +0800

    add hw2.js

---
+++ homework/hw1.js
@@ -0,0 +1,15 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  console.log('ctx=', ctx)
+  console.log('ctx.request=', ctx.request)
+  console.log('url=', ctx.request.url)
+  ctx.response.body = `
+    pathname=${ctx.request.url.pathname} 
+  `;
+});
+
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
