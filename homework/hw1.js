8681402fc1d39b4a7774e8143a06c78170d5f2f4
Leng-Jin <s111110554@student.nqu.edu.tw>
2023-10-19 22:24:44 +0800

    add hw1.js

--- homework/hw1.js
+++ homework/hw1.js
@@ -1,14 +1,36 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
-
-app.use((ctx) => {
-  console.log('ctx=', ctx)
-  console.log('ctx.request=', ctx.request)
-  console.log('url=', ctx.request.url)
+app.use(async (ctx) => {
+  console.log('path=', ctx.request.url.pathname);
   ctx.response.body = `
-    pathname=${ctx.request.url.pathname} 
-  `;
+  <html>
+    <body>
+    </body>
+  </html>`
+
+  if (ctx.request.url.pathname == '/nqu') {
+    ctx.response.body = `
+      <html>
+        <body>
+          <a href="https://www.nqu.edu.tw/">金門大學</a>
+        </body>
+      </html>`
+  } 
+  else if (ctx.request.url.pathname == '/nqu/csie') {
+    ctx.response.body = `
+      <html>
+        <body>
+          <a href="https://csie.nqu.edu.tw/">金門大學資訊工程學系</a>
+        </body>
+      </html>`
+  } 
+  else if (ctx.request.url.pathname == '/to/nqu') {
+    ctx.response.redirect('https://www.nqu.edu.tw/')
+  } 
+  else if (ctx.request.url.pathname == '/to/nqu/csie') {
+    ctx.response.redirect('https://csie.nqu.edu.tw')
+  }
 });
 
 console.log('start at : http://127.0.0.1:8000')
